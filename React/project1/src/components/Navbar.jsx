5cf681e5cbb1d82adcda622b5e8348de1bd7f044
cheguntasrilatha <gsrilathagsrilatha234@gmail.com>
2024-10-27 22:17:12 +0530

    init

--- React/project1/src/components/Navbar.jsx
+++ React/project1/src/components/Navbar.jsx
@@ -1,14 +1,21 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 const Navbar= ({appdata,logo}) => {
     const[stringstate,setStringstate]= useState("s")
     const[numberstate, setNumberstate] = useState(4)
     const[arraystate,setArraystate]= useState(["a",false,0])
-    console.log(numberstate)
-    console.log(stringstate)
-    console.log(arraystate)
+    //console.log(numberstate)
+   // console.log(stringstate)
+    //console.log(arraystate)
+    const [count,setCount] =useState(0);
+    useEffect (() => {
+       //alert('effect rendered')
+       console.log("The count " + count)
+    },[count])
     return(
         <>
-        {appdata.appname} {logo}
+        <button className='w-[6rem] h-[4-rem] bg-slate-500 text-yellow-300' onClick={() => { setCount(count+1) }}>
+            Count {count}
+        </button>
         </>
     )
 }
