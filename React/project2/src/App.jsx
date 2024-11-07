5c9f4b836f87ffa283290e743c5d58c012dc8e29
cheguntasrilatha <gsrilathagsrilatha234@gmail.com>
2024-11-07 21:43:09 +0530

    commit

--- React/project2/src/App.jsx
+++ React/project2/src/App.jsx
@@ -1,35 +1,22 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import Navbar from "./components/Navbar"
+import DefaultExportSample from "./samples/DefaultExportSample"
+import { Module1, Module2 } from "./samples/ModuleExportSample"
 
-function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+const App = () => {
+    const data = {
+        appname: "Hello"
+    }
+    const logodata = "mylogo"
+    const test = "hello world"
+    return (
+        <>
+            <p className="text-yellow-500">{test}</p>
+            <Navbar appdata={data} logo={logodata} />
+            <DefaultExportSample />
+            <Module1 />
+            <Module2 />
+        </>
+    )
 }
 
 export default App
